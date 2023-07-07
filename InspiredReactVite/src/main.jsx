4c2057c06fa77ad745918d020093f875155bfb00
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-07-07 13:46:30 +0300

    feat myone Hello React

--- InspiredReactVite/src/main.jsx
+++ InspiredReactVite/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Привет <strong>React Vite</strong></h1>
-    <App />
+    <h1>Привет React Vite</h1>
   </React.StrictMode>,
 )
