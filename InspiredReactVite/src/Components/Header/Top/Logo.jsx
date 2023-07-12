8b23ff09db386dd43adb7ecda8c806bbb2c721a3
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-07-12 19:30:57 +0300

    feat logo by NavLink

--- InspiredReactVite/src/Components/Header/Top/Logo.jsx
+++ InspiredReactVite/src/Components/Header/Top/Logo.jsx
@@ -1,10 +1,17 @@
-import s from './Top.module.scss';
 import cn from 'classnames';
+import { NavLink } from 'react-router-dom';
+import s from './Top.module.scss';
 import logo from '/src/assets/main-logo.svg';
 
 
 export const Logo = () => (
-  <a className={cn(s.topLink, s.topLogo)} href="/" title="Логотип Inspired">
-    <img className={cn(s.topLogo)} src={logo} alt="Логотип Inspired" />
-  </a>
+  <NavLink className={cn(s.topLink, s.topLogo)} to="/">
+    <img className={cn(s.topLogo)} src={logo} alt="Логотип Inspired" title="Логотип Inspired"/>
+  </NavLink>
 )
+
+// export const Logo = () => (
+//   <a className={cn(s.topLink, s.topLogo)} href="/" title="Логотип Inspired">
+//     <img className={cn(s.topLogo)} src={logo} alt="Логотип Inspired" />
+//   </a>
+// )
