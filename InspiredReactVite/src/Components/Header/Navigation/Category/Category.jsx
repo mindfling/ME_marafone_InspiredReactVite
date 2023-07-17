787c014f1cdae1a87bed9dcbbdbf60d553ef6471
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-07-17 12:33:54 +0300

    feat kids cats

--- InspiredReactVite/src/Components/Header/Navigation/Category/Category.jsx
+++ InspiredReactVite/src/Components/Header/Navigation/Category/Category.jsx
@@ -7,12 +7,15 @@ import { useSelector } from 'react-redux';
 // const location = useLocation();
 // const gender = location.pathname.split('/')[1] || 'women';
 
-export const Category = ({ cats }) => {
-  const activeGender = useSelector((state) => state.navigation.activeGender);
+export const Category = () => {
+  // const activeGender = useSelector((state) => state.navigation.activeGender);
+  // const { activeGender, genderList, categories } = useSelector(state => state.navigation);
+  const activeGender = useSelector(state => state.navigation.activeGender);
+  const categories = useSelector(state => state.navigation.categories);
 
   return (
-    <ul className={cn(s.category, 'category')}>
-      {cats[activeGender].list.map((item) => (
+    <ul className={s.category}>
+      {categories[activeGender]?.list.map((item) => (
         <li key={item.slug} className={s.item}>
           <NavLink
             className={({ isActive }) => cn(s.link, isActive && s.linkActive)}
